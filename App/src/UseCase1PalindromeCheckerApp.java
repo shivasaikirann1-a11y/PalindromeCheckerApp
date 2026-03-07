19e393fccf13a6c37c028d191fd253f175be5ccc
Shiva Sai Kiran <shivasaikirann1@gmail.com>
2026-03-07 10:38:54 +0530

    UC1 - Application Entry and Welcome Message implemented

---
+++ App/src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("Palindrome Checker Application");
+        System.out.println("Application Version : 1.0");
+        System.out.println("=================================");
+
+    }
+}
